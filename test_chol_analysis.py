2fc4592ff48c77e084a9adf5063a735f79115330
amatthews2 <alm143@duke.edu>
2020-04-12 23:18:58 -0400

    changed to get_json

--- test_chol_analysis.py
+++ test_chol_analysis.py
@@ -1,23 +1,33 @@
 
 import pytest
 
-def test_HDLanalysis():
+
+
+@pytest.mark.parametrize("input, output, expected", [
+    (10, 'Normal', True), 
+    (40, 'Borderline', True),
+    (165, 'High', True),
+    (100, 'Low', False),
+])
+
+
+def test_HDLanalysis(input, output, expected):
 # naming convention test_ and name of function being tested
-	from chol_analysis import HDLanalysis
-	answer = HDLanalysis(80)
+	from chol_analysis.py import HDLanalysis
+	answer = HDLanalysis(input)
 	expected = "Normal"
 	assert answer == expected
 
-def test_HDLanalysis_bl():
+def test_HDLanalysis_bl(input, output, expected):
 # naming convention test_ and name of function being tested
-	from chol_analysis import HDLanalysis
-	answer = HDLanalysis(40)
+	from chol_analysis.py import HDLanalysis
+	answer = HDLanalysis(input)
 	expected = "Borderline Low"
 	assert answer == expected
 
 
-def test_LDLanalysis():
-	from chol_analysis import LDLanalysis
-	answer = LDLanalysis(165)
+def test_LDLanalysis(input, output, expected):
+	from chol_analysis.py import LDLanalysis
+	answer = LDLanalysis(input)
 	expected = "High"
 	assert answer == expected
